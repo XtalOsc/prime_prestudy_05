5a6214e9a8c14b4efae4c98ce97395d3ec0aec5c
Xtal Osc <xtalosc@gmx.com>
2016-08-14 16:33:05 -0500

    first commit

---
+++ ObjectAssessment.js
@@ -0,0 +1,7 @@
+var avengersMovie = {
+    name: "Avengers",
+    runtime: "143 mins",
+    releaseYear: 2012,
+    director: "Joss Whedon"
+}
+console.log("Name: " + avengersMovie.name + "\nRun Time: " + avengersMovie.runtime + "\nRelease Year: " + avengersMovie.releaseYear + "\nDirector: " + avengersMovie.director);
