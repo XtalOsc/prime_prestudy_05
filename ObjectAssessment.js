b7794b1c471427f18d3b4558da53e1d30140f771
XtalOsc <xtalosc@gmx.com>
2016-08-15 12:26:18 -0500

    Update ObjectAssessment.js

--- ObjectAssessment.js
+++ ObjectAssessment.js
@@ -1,7 +1,7 @@
-var avengersMovie = {
+avengersMovie = {
     name: "Avengers",
     runtime: "143 mins",
     releaseYear: 2012,
     director: "Joss Whedon"
 }
-console.log("Name: " + avengersMovie.name + "\nRun Time: " + avengersMovie.runtime + "\nRelease Year: " + avengersMovie.releaseYear + "\nDirector: " + avengersMovie.director);
+console.log(avengersMovie);
